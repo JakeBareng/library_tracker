8ca65af3aa4414437545cd21d5264adccfb1239f
JakeBareng <barengjake@gmail.com>
2022-01-02 21:18:06 -0800

    added new entry/card functionality
    new cards created are appended to main area.

--- script.js
+++ script.js
@@ -1,27 +1,68 @@
 const addNew = document.querySelector("#addCard")
 const overlay = document.querySelector("#overlay")
 const submitButn = document.querySelector("#submit")
+const cardSection = document.getElementById("cardSection")
 //form inputs
 const ftitle = document.getElementById("title")
 const fauthor = document.getElementById("author")
 const fpages = document.getElementById("pages")
-const fisRead = document.getElementById("isRead")
+const fisRead = document.querySelectorAll(".isRead")
 
-let myLibrary = [
+const myLibrary = [];
 
-];
 
-function Book(title, author, pages, isRead) {
+
+
+//finish isread button
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-    let title = ftitle.value;
-    let book = new Book(title,author,pages,isRead)
-    myLibrary.push(newBook)
+function addBookToLibrary(title,author,pages) {
+    myLibrary.push(new Book(title,author,pages))
+    appendNewBooks();
+}
+
+function appendNewBooks() {
+    myLibrary.forEach(element => {
+            //card text
+        let cardTitle = document.createElement("h3")
+        cardTitle.innerText = element.title;
+
+        let cardAuthor = document.createElement("h4")
+        cardAuthor.innerText= element.author;
+
+        let cardPages = document.createElement("h4")
+        cardPages.innerText = element.pages;
+
+        //buttons
+        let cardIsReadButn = document.createElement("button")
+        cardIsReadButn.setAttribute("class", "isRead")
+        cardIsReadButn.addEventListener("click", ()=>{
+            //add a toggle
+        })
+
+        let cardRemoveButn = document.createElement("button")
+        cardRemoveButn.setAttribute("class", "remove")
+        cardRemoveButn.addEventListener("click", ()=>{
+            // must remove card
+        })
+        //card
+        let divCard = document.createElement("div");
+        divCard.setAttribute("class", "card")
+
+        //append
+        cardSection.append(divCard)
+        divCard.append(cardTitle,cardAuthor,cardPages,cardIsReadButn,cardRemoveButn)
+
+
+        cardIsReadButn.innerText = "Not Read"
+        cardRemoveButn.innerText = "Remove"
+    });
+
 }
 
 addNew.addEventListener("click", () => {
@@ -31,8 +72,12 @@ addNew.addEventListener("click", () => {
     overlay.style.display = "block flex";
 })
 submitButn.addEventListener("click", () => {
+    if (ftitle.value == "" && fauthor.value=="" && fpages.value == "") {
+        overlay.style.display = "none";
+        return
+    }
+    addBookToLibrary(ftitle.value, fauthor.value, fpages.value)
     overlay.style.display = "none";
 })
-fisRead.addEventListener("click", () =>{
 
-})
+
