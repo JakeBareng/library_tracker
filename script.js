0535d54b6d02504b64b58680e7c425ac2c8d796e
JakeBareng <barengjake@gmail.com>
2022-01-03 13:49:34 -0800

    added - toggle button if book has been read
    toggle button for each card

--- script.js
+++ script.js
@@ -6,28 +6,38 @@ const cardSection = document.getElementById("cardSection")
 const ftitle = document.getElementById("title")
 const fauthor = document.getElementById("author")
 const fpages = document.getElementById("pages")
-const fisRead = document.querySelectorAll(".isRead")
+const fisRead = document.getElementById("isRead")
 
 const myLibrary = [];
 
 
 
-
 //finish isread button
-function Book(title, author, pages) {
+function Book(title, author, pages, isread) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
+    this.isRead = isread;
 }
 
-function addBookToLibrary(title,author,pages) {
-    myLibrary.unshift(new Book(title,author,pages))
+function addBookToLibrary(title,author,pages,isRead) {
+    myLibrary.unshift(new Book(title,author,pages,isRead))
     appendNewBooks();
+    
 }
 
 function removeCard(index) {
     myLibrary.splice(index,1)
+    appendNewBooks();
+}
+
+function isReadToggle(index) {
+    if (myLibrary[index].isRead == false) {
+        myLibrary[index].isRead = true;
+    }
+    else {
+        myLibrary[index].isRead = false;
+    }
     appendNewBooks();
 }
 
@@ -37,9 +47,8 @@ function appendNewBooks() {
     })
 
     myLibrary.forEach(element => {
-            //card text
         let cardTitle = document.createElement("h3")
-        cardTitle.innerText = element.title;
+        cardTitle.innerText = '"' + element.title + '"';
 
         let cardAuthor = document.createElement("h4")
         cardAuthor.innerText = "By: " + element.author;
@@ -47,53 +56,83 @@ function appendNewBooks() {
         let cardPages = document.createElement("h4")
         cardPages.innerText = "pages: " + element.pages;
 
+
         //buttons
         let cardIsReadButn = document.createElement("button")
-        cardIsReadButn.setAttribute("class", "isRead")
-        cardIsReadButn.addEventListener("click", ()=>{
-            //add a toggle
-        })
-
+        if (element.isRead == false) {
+            cardIsReadButn.setAttribute("class", "isRead")
+            cardIsReadButn.innerText = "Not Read"
+        }
+        else {
+            cardIsReadButn.setAttribute("class", "isReadChange")
+            cardIsReadButn.innerText = "This book has been read"
+        }
         let cardRemoveButn = document.createElement("button")
         cardRemoveButn.setAttribute("class", "remove")
         cardRemoveButn.setAttribute("id", myLibrary.indexOf(element))
+        cardRemoveButn.innerText = "Remove"
 
-        //card
         let divCard = document.createElement("div");
         divCard.setAttribute("class", "card")
 
         //append
         cardSection.append(divCard)
         divCard.append(cardTitle,cardAuthor,cardPages,cardIsReadButn,cardRemoveButn)
-        cardIsReadButn.innerText = "Not Read"
-        cardRemoveButn.innerText = "Remove"
+ 
 
-        //remove event
+
+        //button events
         cardRemoveButn.addEventListener("click", ()=>{
             removeCard(cardRemoveButn.id)
+        })
+        cardIsReadButn.addEventListener("click", ()=>{
+            isReadToggle(cardRemoveButn.id)
         })
     });
 
 }
 
+
+
+
+
+
+let isReadValue = false;
+
+fisRead.addEventListener("click", ()=>{
+    isReadValue = false;
+    if(fisRead.getAttribute("class") == "isReadChange"){
+        fisRead.innerText = "Have you read this book? (no)"
+        fisRead.setAttribute('class','isRead')
+    }
+    else {
+        fisRead.setAttribute('class','isReadChange')
+        fisRead.innerText = "Have you read this book? (yes)"
+        isReadValue = true
+    }
+
+})
+
 addNew.addEventListener("click", () => {
     ftitle.value = "";
     fauthor.value = "";
     fpages.value = "";
+
+    fisRead.innerText = "Have you read this book? (no)"
+    fisRead.setAttribute('class','isRead')
+    isReadValue = false;
+
     overlay.style.display = "block flex";
 })
+
 submitButn.addEventListener("click", () => {
-/*     if (ftitle.value == "" && fauthor.value=="" && fpages.value == "") {
-        overlay.style.display = "none";
-        return
-    } */
-    addBookToLibrary(ftitle.value, fauthor.value, fpages.value)
+    addBookToLibrary(ftitle.value, fauthor.value, fpages.value, isReadValue)
     overlay.style.display = "none";
 })
 
-document.addEventListener("keydown",(e)=>{
+overlay.addEventListener("keydown",(e)=>{
     if (e.key == "Enter") {
-        addBookToLibrary(ftitle.value, fauthor.value, fpages.value)
+        addBookToLibrary(ftitle.value, fauthor.value, fpages.value, isReadValue)
         overlay.style.display = "none";
     }
 })
